88fa6b2aeac2b17635202fd3c951fee7cf13a06b
Balázs Vajk-Istiván <balazsvajk2003@gmail.com>
2026-03-28 18:14:31 +0200

    modified:   iac/modules/firehose/outputs.tf

--- iac/modules/firehose/outputs.tf
+++ iac/modules/firehose/outputs.tf
@@ -2,7 +2,7 @@ output "firehose_arn" {
   value = aws_kinesis_firehose_delivery_stream.telemetry_stream.arn
 }
 output "bucket_name" {
-  value = aws_s3_bucket.telemetry_bucket.bucket_domain_name
+  value = aws_s3_bucket.telemetry_bucket.id
 }
 output "bucket_arn" {
   value = aws_s3_bucket.telemetry_bucket.arn

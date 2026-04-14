eea6341489afa641337d4b8277f59a805d360184
Balázs Vajk-Istiván <balazsvajk2003@gmail.com>
2026-04-14 17:00:02 +0300

    deleted:    ../iac/lambda-dist/bridge.zip 	deleted:    ../iac/lambda/backend/controller.py 	deleted:    ../iac/lambda/layers/ur-rtde-layer.zip 	deleted:    ../iac/lambda/sqs/data-sqs.py 	deleted:    ../iac/models/ur3_robot.glb

--- iac/lambda/backend/controller.py
+++
@@ -1,7 +0,0 @@
-import rtde_control
-
-def handler(event, context):
-    # Itt már eléred a könyvtárat
-    # rtde_c = rtde_control.RTDEControlInterface("192.168.1.100")
-    a=4+5
-    return {"status": "success"}
